bfc1b2976b453622b75aab5a5b4ec011ce0478b0
brian <riceb53@gmail.com>
2020-04-27 20:23:59 -0400

    add note selectors and note selector and note editor components

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="note-container">
+    
+    <note-selectors />
+    <!-- note editor was here -->
+    <note-editor />
+  </div>
+</template>
+
+
+<script>
+import NoteSelectors from "./NoteSelectors"
+import NoteEditor from "./NoteEditor"
+
+export default {
+  name: 'note-container',
+  components: {
+    NoteSelectors,
+    NoteEditor
+  }
+}
+</script>
---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="note-selectors">
+    <!-- note seelcor was here -->
+    <note-selector />
+    <note-selector />
+    <note-selector />
+    <note-selector />
+    <note-selector />
+  </div>
+</template>
+
+<script>
+import NoteSelector from './NoteSelector'
+
+export default {
+  name: 'note-selectors',
+  components: {
+    NoteSelector
+  }
+}
+</script>

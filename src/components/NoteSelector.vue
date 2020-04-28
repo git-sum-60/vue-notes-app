6c751e434425017150153b532537642dc84ada25
brian <riceb53@gmail.com>
2020-04-27 21:15:30 -0400

    allow active class to highlight based on click

--- src/components/NoteSelector.vue
+++ src/components/NoteSelector.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="note-selector">
+  <div class="note-selector" v-on:click="setSelectedNote()" v-bind:class="{active: selectedNoteId === note.id}">
     <p class="note-selector-title">{{ note.body }}</p>
     <p class="note-selector-timestamp">Timestamp here...</p>
   </div>
@@ -10,8 +10,14 @@
 export default {
   name: 'note-selector',
   props: [
-    "note"
-  ]
+    "note", "selectedNoteId"
+  ],
+
+  methods: {
+    setSelectedNote: function() {
+      this.$emit("selectNote", this.note)
+    }
+  }
 }
 </script>
 
--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="note-selectors">
     <!-- note seelcor was here -->
-    <note-selector v-for="note in notes" v-bind:key="note.id" v-bind:note="note" />
+    <note-selector v-for="note in notes" v-bind:key="note.id" v-bind:note="note" v-bind:selectedNoteId="selectedNoteId" v-on:selectNote="selectNote" />
   </div>
 </template>
 
@@ -21,7 +21,14 @@ export default {
         { id: 3, body: "This is a third test", timestamp: Date.now() },
         { id: 4, body: "This is a fourth test", timestamp: Date.now() },
         { id: 5, body: "This is a fifth test", timestamp: Date.now() }
-      ]
+      ],
+      selectedNoteId: 4
+    }
+  },
+  methods: {
+    selectNote: function(note) {
+      console.log(note.id)
+      this.selectedNoteId = note.id
     }
   }
 }
